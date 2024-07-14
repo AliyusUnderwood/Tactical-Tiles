467bde9a14798393dca6fa6d380f404bfb15ce86
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-07-13 19:21:34 -0700

    reconstructed backend to utilize graphql

--- server/routes/api/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { signup, login, logout } = require('../../controllers/authController');
-const { authMiddleware } = require('../../utils/auth');
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', authMiddleware, logout);
-
-module.exports = router;
