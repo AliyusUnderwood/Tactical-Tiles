714e74283b9ae5ca9ecbf46f033f353e38bb99c7
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-07-15 20:56:27 -0700

    changed connection.js to accept mongodb atlas MONGO_URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TacticalTilesDB', {
+// mongodb://127.0.0.1:27017/TacticalTilesDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://collinsjosephj:Subaru123!@tacticaltilesdb.r9pwhwe.mongodb.net/?retryWrites=true&w=majority&appName=TacticalTilesDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
