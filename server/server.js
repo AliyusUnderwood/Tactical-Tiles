d6958be7925f0831f0272c840080ae0ca7fb12ec
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-07-15 20:27:12 -0700

    changed server.js to set up mongoose, stuff for mongodb atlas and mongodbURI

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
@@ -16,13 +19,32 @@ const server = new ApolloServer({
   csrfPrevention: false,
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+// Session setup
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
+}));
+
 const startApolloServer = async () => {
   await server.start();
-  
+
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(cors());
-  
+
   app.use('/graphql', expressMiddleware(server, {
     context: async ({ req }) => {
       return authMiddleware({ req });
@@ -37,7 +59,7 @@ const startApolloServer = async () => {
     });
   }
 
-  db.once('open', () => {
+  mongoose.connection.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
