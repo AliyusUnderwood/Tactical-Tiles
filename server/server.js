6b4a72702182ce1c920facdf14635b04d59cee3a
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-07-16 19:10:10 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,39 +1,38 @@
 const express = require('express');
+const path = require('path');
+// Import the ApolloServer class
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
+// Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: true,
-  csrfPrevention: false,
 });
 
+const app = express();
+
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  app.use(cors());
   
   app.use('/graphql', expressMiddleware(server, {
-    context: async ({ req }) => {
-      return authMiddleware({ req });
-    }
+    context: authMiddleware
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
@@ -45,4 +44,5 @@ const startApolloServer = async () => {
   });
 };
 
+// Call the async function to start the server
 startApolloServer();
