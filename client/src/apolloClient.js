a812858e59ef868843887a97c1209f734abd34c8
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-07-18 12:42:09 -0700

    changed apolloClient.js per Steve Calla suggestion

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+//  uri: 'http://localhost:3001/graphql',
+    uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
